c1a44f8f45875198a73b4eeff6f733fad6184c08
rmiyagi511 <rick_miyagi@brown.edu>
2015-09-19 14:08:59 -0700

    myo init
    myo

--- app/src/main/java/com/rickmiyagi/flex/MainActivity.java
+++ app/src/main/java/com/rickmiyagi/flex/MainActivity.java
@@ -1,17 +1,62 @@
 package com.rickmiyagi.flex;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
+import com.thalmic.myo.scanner.ScanActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Hub hub = Hub.getInstance();
+        if (!hub.init(this)) {
+            Log.e(TAG, "Could not initialize the Hub.");
+            finish();
+            return;
+        }
+
+        Intent intent = new Intent(this, ScanActivity.class);
+        this.startActivity(intent);
+
+        Hub.getInstance().setLockingPolicy(Hub.LockingPolicy.STANDARD);
+        Hub.getInstance().addListener(mListener);
+
     }
+
+    private DeviceListener mListener = new AbstractDeviceListener() {
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            Toast.makeText(MainActivity.this, "Myo Connected!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onDisconnect(Myo myo, long timestamp) {
+            Toast.makeText(MainActivity.this, "Myo Disconnected!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPose(Myo myo, long timestamp, Pose pose) {
+            Toast.makeText(MainActivity.this, "Pose: " + pose, Toast.LENGTH_SHORT).show();
+
+            //TODO: Do something awesome.
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
